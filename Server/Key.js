f2a6a4c8a6a6b88bc4c44880cb20f17e3cd9b80d
Tuan <pmtuan.tp@gmail.com>
2023-12-02 16:14:18 +0700

    Add some library, reconnect database, implement some home page stuff

--- Server/Key.js
+++ Server/Key.js
@@ -1,4 +1,5 @@
 module.exports={
-    MONGOURI:"mongodb+srv://VidushiGandhi:YhbuK3ov4gZHlJxG@cluster0.x9v8k85.mongodb.net/?retryWrites=true&w=majority",
+    // MONGOURI:"mongodb+srv://VidushiGandhi:YhbuK3ov4gZHlJxG@cluster0.x9v8k85.mongodb.net/?retryWrites=true&w=majority",
+    MONGOURI: "mongodb+srv://tuanpham12:thisisapassword@cluster0.tdyfggx.mongodb.net/?retryWrites=true&w=majority",
     JWT_SECRET:"gdjhvvdhjddhdbhdbjhonsbdvuhbqwnkqxsab"
 }
