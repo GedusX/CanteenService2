129c3cc51169004d5eb2ab43f2d2b63b70cf1dc1
Tuan <pmtuan.tp@gmail.com>
2023-12-06 22:25:10 +0700

    adding some user api

--- Server/routes/Cart.js
+++ Server/routes/Cart.js
@@ -5,6 +5,14 @@ const requireLogin  = require('../middleware/requireLogin')
 const Cart = mongoose.model("Cart")
 const CartItem = mongoose.model("CartItem")
 
+function getInUseCartId(req){
+
+
+}
+function getAllCartId(req){
+
+
+}
  
 
 router.get('/mycart',requireLogin,(req,res)=>{
@@ -49,8 +57,92 @@ router.get('/mycart',requireLogin,(req,res)=>{
 })
 
 
+router.put('/addToCart',requireLogin,(req,res)=>{
+  var cart_id = req.user.addCart   
+  if (cart_id.length === 0){
+      const newCart = new Cart({
+          inUse:true,
+      });
+      newCart.save((err, savedCart) => {
+          if (err) {
+            return res.status(500).json({ error: err });
+          }
+          User.findByIdAndUpdate(
+          req.user._id,
+          {
+              $push: { addCart: savedCart._id },
+          },
+          {
+              new: true
+          },
+          (err, updatedUser) => {
+              if (err) {
+              return res.status(500).json({ error: err });
+              }
+              cart_id = savedCart._id;
+          }
+          );
+      });
+  }
+  CartItem.countDocuments({ itemPost: req.body.postId, cartBy: cart_id[0]})
+  .then(count => {
+      if (count > 0) {
+        // Item already exists in the cart
+        return CartItem.findOne({ itemPost: req.body.postId, cartBy: cart_id[0] });
+      } else {
+        // Item doesn't exist, create a new CartItem
+        if (req.body.amount!=0){
+          const newItem = new CartItem({
+              itemPost: req.body.postId,
+              amount: req.body.amount ? req.body.amount : 1,
+              cartBy: cart_id[0],
+          });
+          return newItem.save()
+              .then(savedItem => {
+              // Update the Cart with the new item
+              return Cart.findByIdAndUpdate(
+                  cart_id[0],
+                  {
+                  $push: { itemPost: savedItem._id },
+                  },
+                  {
+                  new: true,
+                  }
+              ).exec();
+              });
+        }
+      }
+    })
+    .then(result => {
+      if (result && result.amount) {
+          if (req.body.amount == 0){
+              Cart.findByIdAndUpdate(result.cartBy,{
+                  $pull:{itemPost: result._id}
+              }).exec();
+              CartItem.findByIdAndDelete(result._id).exec();
+              res.status(200).json({ message: 'Item remove'});
+          }else if (result.amount != req.body.amount){
+              CartItem.findByIdAndUpdate(result._id,{
+                  $set:{amount : req.body.amount}
+              }).exec();
+              res.status(200).json({ message: 'Item amount change' });
+          } else{
+              res.status(200).json({ message: 'This item is already exist'});
+          }
+      } else if (req.body.amount == 0) {
+          res.status(200).json({ message: 'The amount can not be 0'}); 
+      } else {
+      console.log(result);
+      res.status(200).json({ message: 'Item added to cart successfully' });
+      }
+    })
+});
 
   
+
+
+
+
 router.post("/shipping",requireLogin,(req,res)=>{
 
   const {address,sector,city,phoneno  }=req.body
