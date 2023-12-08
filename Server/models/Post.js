771bfa093ce4bd6f4e051f724e351c865f24d440
Nguyen Trong Tin <tin.nguyen2115011@hcmut.edu.vn>
2023-12-08 13:02:32 +0700

    product description

--- Server/models/Post.js
+++ Server/models/Post.js
@@ -19,6 +19,10 @@ const postSchema =new mongoose.Schema({
         text:String,
         postedBy:{type:ObjectId,ref:"User"}
     }],
+    desc:{
+        type:String,
+        required:true
+    },
     postedBy:{
         type:ObjectId,
         ref:"User"
